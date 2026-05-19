f5afcd572dca28d5de9b76a39518b49096be7905
castillo-abm <alanbertincastillomartinez@gmail.com>
2026-05-19 10:26:33 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,48 @@
-const slider=document.getElementById("slider");
-const xray=document.getElementById("xray");
+const btn = document.getElementById("btn");
+const rayo = document.getElementById("rayo");
+const xray = document.getElementById("xray");
+const flash = document.getElementById("flash");
 
-slider.addEventListener("input", ()=>{
-    xray.style.opacity=slider.value/100;
+let animando = false;
+
+btn.addEventListener("click", () => {
+
+    if (animando) return;
+    animando = true;
+
+    // reset
+    rayo.style.transition = "none";
+    rayo.style.right = "-300px";
+    rayo.style.opacity = "1";
+
+    xray.style.opacity = "0";
+    flash.style.opacity = "0";
+
+    void rayo.offsetWidth;
+
+    //SOLO dentro de la mitad
+    rayo.style.transition = "right 0.9s ease, opacity 0.4s ease";
+    rayo.style.right = "0px";
+
+    // flash
+    setTimeout(() => {
+        flash.style.opacity = "1";
+    }, 700);
+
+    // RX aparece
+    setTimeout(() => {
+        xray.style.opacity = "1";
+        flash.style.opacity = "0";
+    }, 900);
+
+    //rayo desaparece
+    setTimeout(() => {
+        rayo.style.opacity = "0";
+    }, 1100);
+
+    setTimeout(() => {
+        xray.style.opacity = "0";
+        animando = false;
+    }, 1800);
+
 });
